883c90149eab1067cff24f7ab4b50ad9d60e69fd
Classic <Ivan2002dav@gmail.com>
2019-07-10 03:03:25 +0300

    Code update

--- src/App.js
+++ src/App.js
@@ -1,153 +1,82 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react'
+import {images as imagesArray} from './images'
 import Granim from 'granim'
 
-class App extends Component {
-  state = {
-    images: [
-      {
-        title: 'First',
-        image_key: Math.random(),
-        loaded: false
-      },
-      {
-        title: 'Second',
-        image_key: Math.random(),
-        loaded: false
-      },
-      {
-        title: 'Third',
-        image_key: Math.random(),
-        loaded: false
-      },
-      {
-        title: 'Fourth',
-        image_key: Math.random(),
-        loaded: false
-      },
-      {
-        title: 'Fifth',
-        image_key: Math.random(),
-        loaded: false
-      },
-      {
-        title: 'Sixth',
-        image_key: Math.random(),
-        loaded: false
-      },
-      {
-        title: 'Seventh',
-        image_key: Math.random(),
-        loaded: false
-      },
-      {
-        title: 'Eighth',
-        image_key: Math.random(),
-        loaded: false
-      },
-      {
-        title: 'Ninth',
-        image_key: Math.random(),
-        loaded: false
-      },
-      {
-        title: 'Tenth',
-        image_key: Math.random(),
-        loaded: false
-      },
-      {
-        title: 'Eleventh',
-        image_key: Math.random(),
-        loaded: false
-      },
-      {
-        title: 'Twelfth',
-        image_key: Math.random(),
-        loaded: false
+export default () => {
+  const [images, setImages] = useState(imagesArray)
+  
+  useEffect(() => {
+    new Granim({
+      element: '#canvas-basic',
+      name: 'basic-gradient',
+      direction: 'left-right', // 'diagonal', 'top-bottom', 'radial'
+      opacity: [1, 1],
+      isPausedWhenNotInView: true,
+      states : {
+          "default-state": {
+              gradients: [
+                  ['#AA076B', '#61045F'],
+                  ['#02AAB0', '#00CDAC'],
+                  ['#DA22FF', '#9733EE']
+              ]
+          }
       }
-    ]
-  }
-
-  componentDidMount(){
-      var granimInstance = new Granim({
-        element: '#canvas-basic',
-        name: 'basic-gradient',
-        direction: 'left-right', // 'diagonal', 'top-bottom', 'radial'
-        opacity: [1, 1],
-        isPausedWhenNotInView: true,
-        states : {
-            "default-state": {
-                gradients: [
-                    ['#AA076B', '#61045F'],
-                    ['#02AAB0', '#00CDAC'],
-                    ['#DA22FF', '#9733EE']
-                ]
-            }
-        }
-    });
-  }
-
-  onLoadImage = (i) => {
-    let new_state = Object.assign({}, this.state, {
-      images: this.state.images.map((image, index) => {
-        if (index === i) {
-          return Object.assign({}, image, {
-            loaded: true
-          })
-        }
-        return image
-      })
     })
-    this.setState(new_state)
-  }
+  }, [])
 
-  LoadNewImage = (i) => {
-    let new_state = Object.assign({}, this.state, {
-      images: this.state.images.map((image, index) => {
-        if (index === i) {
-          return Object.assign({}, image, {
-            image_key: Math.random(),
-            loaded: false
-          })
-        }
-        return image
-      })
+  const onLoadImage = (i) => {
+    let newImages = images.map((image, index) => {
+      if (index === i) {
+        return Object.assign({}, image, {
+          loaded: true
+        })
+      }
+      return image
     })
-    this.setState(new_state)
+    setImages(newImages)
   }
 
-  render() {
-    console.log(this.state.images)
-    return (
-      <div className="App ">
-        <canvas id="canvas-basic" />
+  const loadNewImage = (i) => {
+    let newImages = images.map((image, index) => {
+      if (index === i) {
+        return Object.assign({}, image, {
+          image_key: Math.random(),
+          loaded: false
+        })
+      }
+      return image
+    })
+    setImages(newImages)
+  }
 
-        <div className="container pt-3 text-center">
-          <h1 className="canvas-text mt-2">RateEm</h1>
-        </div>
+  return (
+    <div className="App ">
+      <canvas id="canvas-basic" />
 
-        <div className="content container mb-5">
-          <div className="row">
-
-            {this.state.images.map((image, i) => {
-              return(
-                <div className="col-lg-3 mt-5" key={i} onClick={() => this.LoadNewImage(i)}>
-                  <div className="content-piece text-center container-fluid">
-                    <h1>{image.title}</h1>
-                    {!image.loaded ?
-                      <div className="img-fluid image-loading mx-auto"></div> : ''
-                    }
-                    <img className={!image.loaded ? "img-fluid d-none" : "img-fluid"} src={'https://picsum.photos/200/300/?random' + image.image_key} onLoad={() => this.onLoadImage(i)} ref="image"/>
-                  </div>
-                </div>
-              )
-            })}
-
-          </div>
-        </div>
-
+      <div className="container pt-3 text-center">
+        <h1 className="canvas-text mt-2">Granim + React</h1>
       </div>
-    );
-  }
-}
 
-export default App;
+      <div className="content container mb-5">
+        <div className="row">
+
+          {images && images.map((image, i) => {
+            return(
+              <div className="col-lg-3 mt-5" key={i} onClick={() => loadNewImage(i)}>
+                <div className="content-piece text-center container-fluid">
+                  <h1>{image.title}</h1>
+                  {!image.loaded ?
+                    <div className="img-fluid image-loading mx-auto"></div> : ''
+                  }
+                  <img className={!image.loaded ? "img-fluid d-none" : "img-fluid"} src={'https://picsum.photos/200/300/?random' + image.image_key} onLoad={() => onLoadImage(i)}/>
+                </div>
+              </div>
+            )
+          })}
+
+        </div>
+      </div>
+
+    </div>
+  )
+}
---
+++ src/images.js
@@ -0,0 +1,62 @@
+export const images = [
+  {
+    title: 'First',
+    image_key: Math.random(),
+    loaded: false
+  },
+  {
+    title: 'Second',
+    image_key: Math.random(),
+    loaded: false
+  },
+  {
+    title: 'Third',
+    image_key: Math.random(),
+    loaded: false
+  },
+  {
+    title: 'Fourth',
+    image_key: Math.random(),
+    loaded: false
+  },
+  {
+    title: 'Fifth',
+    image_key: Math.random(),
+    loaded: false
+  },
+  {
+    title: 'Sixth',
+    image_key: Math.random(),
+    loaded: false
+  },
+  {
+    title: 'Seventh',
+    image_key: Math.random(),
+    loaded: false
+  },
+  {
+    title: 'Eighth',
+    image_key: Math.random(),
+    loaded: false
+  },
+  {
+    title: 'Ninth',
+    image_key: Math.random(),
+    loaded: false
+  },
+  {
+    title: 'Tenth',
+    image_key: Math.random(),
+    loaded: false
+  },
+  {
+    title: 'Eleventh',
+    image_key: Math.random(),
+    loaded: false
+  },
+  {
+    title: 'Twelfth',
+    image_key: Math.random(),
+    loaded: false
+  }
+]
